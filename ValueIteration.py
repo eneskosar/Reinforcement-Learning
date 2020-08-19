669ee5ddb5db34b6b9ab8e6af0fa66c90c10f65e
eneskosar <ekosar13@ku.edu.tr>
2020-08-19 20:24:22 +0300

    asd

--- ValueIteration.py
+++ ValueIteration.py
@@ -16,8 +16,8 @@ transitionKernel = {
     "['B',0,'B']": 0.05
 }
 
-mu = 0.95
-beta = 0.5
+mu = 0.5
+beta = 0.8
 
 def cost(x,u):
     if int(x == "G") & u == 1:
@@ -36,20 +36,36 @@ while True:
     delta = 0
     for s in states:
         best_EV = 999
+        best_cost = 999
+        best_total_Cost=999
         for a in actions:
             probGood = transitionKernel[f"['{s}',{a},'G']"]
             probBad = transitionKernel[f"['{s}',{a},'B']"]
-            EV = probGood*StateValues["G"] + probBad*StateValues["B"]
 
-            best_EV = min(best_EV, EV)
-        StateValues[s] = cost(s,a) + beta*best_EV
+            if (a == 0):
+                EV0 = probGood * StateValues["G"] + probBad * StateValues["B"]
+                cost0 = cost(s, a)
+                total_cost0 = EV0 + cost0
+            if (a == 1):
+                EV1 = probGood * StateValues["G"] + probBad * StateValues["B"]
+                cost1 = cost(s, a)
+                total_cost1 = EV1 + cost1
+
+        if (total_cost1 > total_cost0):
+            best_EV = EV0
+            best_cost = cost0
+        elif (total_cost0 >= total_cost1):
+            best_EV = EV1
+            best_cost = cost1
+
+        StateValues[s] = best_cost + beta*best_EV
         print("***********")
         print(StateValues)
         # calculate maximum difference in value
         delta = max(delta, abs(StateValues[s] - oldStateValues[s]))
 
     # check for convergence, if values converged then return V
-    if delta < 0.001:
+    if delta < 0.0001:
         break
 print("**************************")
 print(StateValues)
